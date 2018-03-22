15d715b0b02ede1b2d4188b7cd8dbef4a8599aa2
Breno Batista <brenojab@gmail.com>
2018-03-22 15:51:05 -0300

    Conclusão aula 14 - inferência de tipos

--- Angular/Typescript/app.js
+++ Angular/Typescript/app.js
@@ -1,8 +1,19 @@
-"use strict";
-// Inferência de tipo, passando o tipo
-var ola = function (nome) {
-    console.log("Yeah, " + nome + "!");
-};
-ola("Breno");
+// passando o tipo
+/*
+let ola = (nome: string, sobrenome :string) => {
+    console.log("Yeah, " + nome + " " + sobrenome + "!")
+}
+
+ola("Breno", "Batista")
+*/
+/*** Inferência de tipo (é possível tipificar as variáveis) ***/
+// Tipos conhecidos
+var mensagem = "Welcome!";
+var temporadasFriends = 10;
+var estudandoAngular = true;
+// Formas de declarar arrays
+var listaDeFrutas = ['Uva', 'Banana', 'Abacaxi'];
+var notasDasProvas = [7.5, 8, 9];
+var notasDasProvas2 = [7.5, 8, 9];
 // Verificação estática
 // Suporte a classes e interfaces
--- Angular/Typescript/app.ts
+++ Angular/Typescript/app.ts
@@ -1,11 +1,24 @@
 // passando o tipo
+/*
 let ola = (nome: string, sobrenome :string) => {
     console.log("Yeah, " + nome + " " + sobrenome + "!")
 }
 
 ola("Breno", "Batista")
+*/
 
-// Inferência de tipo,
+/*** Inferência de tipo (é possível tipificar as variáveis) ***/
+
+// Tipos conhecidos
+let mensagem: string = "Welcome!"
+let temporadasFriends: number = 10
+let estudandoAngular: boolean = true
+
+// Formas de declarar arrays
+let listaDeFrutas: string[] = ['Uva', 'Banana', 'Abacaxi']
+let notasDasProvas: Array<Number> = [7.5, 8, 9]
+let notasDasProvas2: Number[] = [7.5, 8, 9]
+
 
 // Verificação estática
 
